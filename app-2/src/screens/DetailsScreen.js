3db24ad1037cdfda389a9ae9c28d49a6bdf72741
Kahlil B <kahlilbeasley@Kahlils-MacBook-Air.local>
2021-07-13 21:52:22 -0700

    api calls

--- app-2/src/screens/DetailsScreen.js
+++ app-2/src/screens/DetailsScreen.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 const DetailsScreen = ( {navigation, route} ) => {
     console.log(route);
     const movie = route.params.movie;
-    
+
+    // State for API data
+    const [movieDetails, setMovieDetails] = useState(null);
+
+    useEffect(() => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://www.omdbapi.com/?apikey=64af7da4&t=star+wars&y=1977');
+      xhr.send()
+      xhr.onload = () => {
+        if(xhr.status === 200){
+          // console.log('HERE?!?!?', xhr.responseText)
+          let response = JSON.parse(xhr.response)
+
+          // set movie details
+          setMovieDetails(response)
+        }else{
+          console.error(`ERROR! ${xhr.status}`)
+        }
+      }
+
+      xhr.onerror = () => {
+        console.log('Error')
+      }
+    }, [])
+
     return (
       <View style={styles.mainView}>
-        <Text style={{fontSize: 20}}>{movie.title} ({movie.release})</Text>
-        <Text style={{fontSize: 100}} >{movie.screenNumber}</Text>
+        <Text>{movieDetails ? movieDetails.Title : ''}</Text>
+        <Text>{movieDetails ? movieDetails.Released : ''}</Text>
+        <Text>{movieDetails ? movieDetails.Plot :  ''}</Text>
+
+
+        {/* <Text style={{fontSize: 20}}>{movie.title} ({movie.release})</Text>
+        <Text style={{fontSize: 100}} >{movie.screenNumber}</Text> */}
         <Button
           title="Go to Image"
           onPress={() => {navigation.navigate("ImageView")}}/>
