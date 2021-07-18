74c0e972f3255092f84c90ae4a4affa6a72e2833
Kahlil B <kahlilbeasley@Kahlils-MacBook-Air.local>
2021-07-17 21:09:41 -0700

    web socket example, async storage, animations

--- app-chat/components/Chat.tsx
+++ app-chat/components/Chat.tsx
@@ -9,6 +9,8 @@ import {
 } from "react-native";
 import { ChatItem, RenderChatItem } from "./ChatItem";
 import Styles from "./Styles";
+import Socket from './Socket';
+import { useEffect } from "react";
 
 type ChatProps = {
   username: string;
@@ -18,6 +20,24 @@ type ChatProps = {
 const Chat = ({ username, image }: ChatProps) => {
   let [chatInput, setChatInput] = useState("");
   let [chatItemList, setChatItemList] = useState<ChatItem[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (Socket.state == "Disconnected") {
+          await Socket.start();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+    Socket.on("ReceiveMessage", (chatItem) => {
+      setChatItemList((chatItemList) => {
+        if (chatItemList.find((i) => i.id == chatItem.id)) return chatItemList;
+        return [...chatItemList, chatItem];
+      });
+    });
+  }, []);
 
   const renderItem: ListRenderItem<ChatItem> = ({ item }) => (
     <RenderChatItem chatItem={item} username={username}></RenderChatItem>
@@ -40,17 +60,14 @@ const Chat = ({ username, image }: ChatProps) => {
         ></TextInput>
         <Button
           title="Send"
-          onPress={() => {
-            setChatItemList([
-              ...chatItemList,
-              {
-                id: Math.random().toString(36).substring(7),
-                text: chatInput,
-                image: image,
-                timeStamp: Date.now(),
-                by: username,
-              },
-            ]);
+          onPress={async () => {
+            await Socket.invoke("SendMessage", {
+              id: Math.random().toString(36).substring(7),
+              text: chatInput,
+              image: image,
+              timeStamp: Date.now(),
+              by: username,
+            });
             setChatInput("");
           }}
         ></Button>
